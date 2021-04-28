470f48eff7f8f19d2f55b89d9137c7a7e01b5ac6
shahryarsz <szshahryar@gmail.com>
2021-04-28 15:56:05 +0430

    Storage class added

---
+++ src/com/company/Storage.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Storage {
+
+    protected ArrayList<Card> storeCards;
+
+    public Storage(){
+        storeCards = new ArrayList<>();
+    }
+}
